6a738c331d7f63a14d2af49814072f2e43b2446c
Luis Casas <luisscasas45@gmail.com>
2025-07-26 13:30:36 -0600

    FEAT/ add Agenda view and integrate it into the router

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,12 @@
 import { createRouter, createWebHistory } from "vue-router";
 import Login from "../views/Login.vue";
 import Pacientes from "../views/Pacientes.vue";
+import Agenda from "../views/Agenda.vue";
 
 const routes = [
   { path: "/", name: "Login", component: Login },
   { path: "/pacientes", name: "Pacientes", component: Pacientes },
+  { path: "/agenda", name: "Agenda", component: Agenda },
 ];
 
 const router = createRouter({
